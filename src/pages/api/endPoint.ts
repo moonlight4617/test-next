1cf9e7bfd8b844bcce372b3b5c6b9edc988fbf7f
moonlight4617 <moonlight4_6_17@yahoo.co.jp>
2023-07-09 16:17:00 +0900

    型エラー対応

--- src/pages/api/endPoint.ts
+++ src/pages/api/endPoint.ts
@@ -9,30 +9,30 @@ type Props = {}
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 
   try {
-  const result = await axios.get(
-    publicRuntimeConfig.hatenaApi,
-    // "https://blog.hatena.ne.jp/fluid_27/fluid-27.hatenablog.com/atom/entry",
-    {
-      auth: {
-        // username: "fluid_27",
-        username: process.env.HATENA_AUTH_USER,
-        password: process.env.HATENA_AUTH_PATH
-      },
-      headers: {
-        'content-type': 'application/json',
-      },
-    }
-  )
-  return res.status(200).json(result.data);
-}catch (error) {
-  console.log("Error:", error);
+    const result = await axios.get(
+      publicRuntimeConfig.hatenaApi,
+      // "https://blog.hatena.ne.jp/fluid_27/fluid-27.hatenablog.com/atom/entry",
+      {
+        auth: {
+          // username: "fluid_27",
+          username: process.env.HATENA_AUTH_USER || "",
+          password: process.env.HATENA_AUTH_PATH || ""
+        },
+        headers: {
+          'content-type': 'application/json',
+        },
+      }
+    )
+    return res.status(200).json(result.data);
+  } catch (error) {
+    console.log("Error:", error);
     return res.status(500).json({ message: "An error occurred" });
-}
-    // .then((data) => {
-      // const portfolioArray = data.data.data.map((portfolio: any, index: number) => {
-      //   return index < 3 ? { ...portfolio.attributes } : null
-      // });
-      // return portfolioArray
+  }
+  // .then((data) => {
+  // const portfolioArray = data.data.data.map((portfolio: any, index: number) => {
+  //   return index < 3 ? { ...portfolio.attributes } : null
+  // });
+  // return portfolioArray
   //   })
   //   .catch(err => {
   //     console.log("err:", err);
